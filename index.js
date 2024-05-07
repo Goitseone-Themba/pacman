9f2e1a9a43f4eb1d886455432adbe6dba4364298
Goitseone_Themba <97857682+Goitseone-Themba@users.noreply.github.com>
2024-05-07 05:18:01 +0200

    create initial html and js file, index.html and index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const canvas = document.querySelector('canvas')
+
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Boundary {
+    constructor() {
+        
+    }
+}
